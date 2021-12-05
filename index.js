67b65fd9debd35d162c873a066441d0ecbc74812
zeleney30 <michaelzeleney@gmail.com>
2021-12-04 20:17:20 -0500

    CSS

--- index.js
+++ index.js
@@ -1,34 +1,20 @@
-// npm install express
 const express = require("express");
 
-// other modules
 const path = require("path");
 const ejs = require("ejs");
 
-// Initialize express framework
 const app = express();
 
-// Initialize Body Parser Middleware to parse data sent by users in the request object
 app.use(express.json());
-app.use(express.urlencoded({ extended: false })); // to parse HTML form data
+app.use(express.urlencoded({ extended: false }));
 
-// Initialize Handlebar Middleware
-app.set("views", "./views");
 app.set('view engine', 'ejs');
 
-/*app.get("/", (req, res) => {
-  // by default it looks for .ejs files in the views folder
-  res.render("index", { name: "Michael" });
-});*/
-
-
 app.get('', (req, res) => {
-  //res.sendFile(__dirname + '/views/index.html');
   res.render('index');
 })
 
 app.get('/index', (req, res) => {
-  //res.sendFile(__dirname + '/views/index.html');
   res.render('index');
 })
 
@@ -48,7 +34,8 @@ app.get('/trivia', (req, res) => {
   res.render('trivia');
 })
 
-// Start server on a PORT
+
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
